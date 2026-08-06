1214e9fb57f673063ccb877b9917581962c88b31
Jagritirai586 <jagriti.pro@gmail.com>
2026-08-06 23:02:22 +0530

    Add dark mode and local storage support

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ detailButtons.forEach(button => {
         }
     });
 });
+
+// Dark Mode Toggle
+
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        themeBtn.textContent = "☀️";
+    }else{
+        themeBtn.textContent = "🌙";
+    }
+
+});
